8b820edc28676b1629046c2f99a730930f87fdb5
hp <ajss.sravan@gmail.com>
2020-04-08 13:38:04 +0530

    Details stored in firebasefirestore and Emergency contacts layout prepared

--- app/src/main/java/com/example/weprotectu/SignUp.java
+++ app/src/main/java/com/example/weprotectu/SignUp.java
@@ -1,5 +1,6 @@
 package com.example.weprotectu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -14,9 +15,14 @@ import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignUp extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     String p;
@@ -30,6 +36,7 @@ public class SignUp extends AppCompatActivity implements AdapterView.OnItemSelec
     FirebaseAuth fAuth;
     FirebaseFirestore firestore;
     DocumentReference docref;
+    String uid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,12 +54,49 @@ public class SignUp extends AppCompatActivity implements AdapterView.OnItemSelec
         bgp1.setOnItemSelectedListener(this);
         b=(Button)findViewById(R.id.con1);
         rg=(RadioGroup)findViewById(R.id.gender);
-
+        firestore=FirebaseFirestore.getInstance();
+        fAuth=FirebaseAuth.getInstance();
+        uid=fAuth.getCurrentUser().getUid();
+        docref=firestore.collection("user").document(uid);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 usernameres=username.getText().toString();
                 Toast.makeText(SignUp.this, "details: "+usernameres+" "+bloodgroupres+" "+genderres, Toast.LENGTH_SHORT).show();
+                if(usernameres.isEmpty()) {
+                    username.setError("Fill the User Name");
+                    username.requestFocus();
+                }
+                else if(bloodgroupres.isEmpty())
+                {
+                    Toast.makeText(SignUp.this, "Select Your Blood Group", Toast.LENGTH_SHORT).show();
+                }
+                else if(genderres.isEmpty())
+                {
+                    Toast.makeText(SignUp.this, "Select Gender", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Map<String,Object> user=new HashMap<>();
+                    user.put("username",usernameres);
+                    user.put("bloodgroup",bloodgroupres);
+                    user.put("gender",genderres);
+                    user.put("Phonenumber",p);
+                    docref.set(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                               Intent i4=new Intent(SignUp.this,EmergencyContacts.class);
+                                startActivity(i4);
+                                Toast.makeText(SignUp.this, "Data added successfully", Toast.LENGTH_SHORT).show();
+                            }
+                            else
+                            {
+                                Toast.makeText(SignUp.this, "Unable to add data", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
             }
         });
     }
--- app/src/main/java/com/example/weprotectu/VerifyOtp.java
+++ app/src/main/java/com/example/weprotectu/VerifyOtp.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.AuthResult;
@@ -20,13 +21,18 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.concurrent.TimeUnit;
 
 public class VerifyOtp extends AppCompatActivity {
     EditText enterotp1;
     Button b;
-    FirebaseAuth mAuth;
+    FirebaseAuth fAuth;
+    DocumentReference docref;
+    FirebaseFirestore fstore;
     String p;
     ProgressBar pb1;
     TextView state1;
@@ -38,12 +44,13 @@ public class VerifyOtp extends AppCompatActivity {
         b=(Button)findViewById(R.id.continu);
         pb1=(ProgressBar)findViewById(R.id.pb);
         state1=(TextView)findViewById(R.id.state);
-        mAuth=FirebaseAuth.getInstance();
         Intent i=getIntent();
         final String c=i.getStringExtra("co");
         String ar[]=c.split(" ");
         final String codeSent=ar[0];
         p=ar[1];
+        fAuth=FirebaseAuth.getInstance();
+        fstore=FirebaseFirestore.getInstance();
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -75,16 +82,13 @@ public class VerifyOtp extends AppCompatActivity {
 
     }
     private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
-        mAuth.signInWithCredential(credential)
+        fAuth.signInWithCredential(credential)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            Toast.makeText(VerifyOtp.this, "Verification Successful", Toast.LENGTH_SHORT).show();
-                            Intent i2=new Intent(VerifyOtp.this,SignUp.class);
-                            i2.putExtra("phnumber",p);
-                            startActivity(i2);
-                            finish();
+                            checkProfile();
+
                         } else {
                             if (task.getException() instanceof FirebaseAuthInvalidCredentialsException)
                             {
@@ -100,5 +104,35 @@ public class VerifyOtp extends AppCompatActivity {
                     }
 
                 });
+    }
+
+    private void checkProfile() {
+        docref=fstore.collection("user").document(fAuth.getCurrentUser().getUid());
+        docref.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if(documentSnapshot.exists()){
+                    Toast.makeText(VerifyOtp.this, "Already a registered User", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(VerifyOtp.this, "Verification Successful", Toast.LENGTH_SHORT).show();
+                    Intent i2=new Intent(VerifyOtp.this,SignUp.class);
+                    i2.putExtra("phnumber",p);
+                    startActivity(i2);
+                    finish();
+                }
+            }
+
+        });
+    }
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        if(fAuth.getCurrentUser()!=null)
+        {
+            checkProfile();
+        }
     }
 }
