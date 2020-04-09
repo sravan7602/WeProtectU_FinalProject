558cd6fc0d8e07aab684484084aba406b3079334
hp <ajss.sravan@gmail.com>
2020-04-10 03:58:47 +0530

    Emergency contacts added

--- app/src/main/java/com/example/weprotectu/EmergencyContacts.java
+++ app/src/main/java/com/example/weprotectu/EmergencyContacts.java
@@ -1,7 +1,6 @@
 package com.example.weprotectu;
 
 import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -12,18 +11,20 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class EmergencyContacts extends AppCompatActivity {
     EditText mn1,mn2,mn3,mn4;
+    TextView c1,c2,c3,c4;
     int res=0;
-    ImageView add1;
+    ImageView add1,add2,add3,add4;
+    ImageView remove1,remove2,remove3,remove4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,11 +33,22 @@ public class EmergencyContacts extends AppCompatActivity {
         mn2 = (EditText) findViewById(R.id.mobilenumber2);
         mn3 = (EditText) findViewById(R.id.mobilenumber3);
         mn4 = (EditText) findViewById(R.id.mobilenumber4);
+        c1=(TextView)findViewById(R.id.contact1);
+        c2=(TextView)findViewById(R.id.contact2);
+        c3=(TextView)findViewById(R.id.contact3);
+        c4=(TextView)findViewById(R.id.contact4);
+        remove1=(ImageView)findViewById(R.id.remove1);
+        remove2=(ImageView)findViewById(R.id.remove2);
+        remove3=(ImageView)findViewById(R.id.remove3);
+        remove4=(ImageView)findViewById(R.id.remove4);
         mn1.setEnabled(false);
         mn2.setEnabled(false);
         mn3.setEnabled(false);
         mn4.setEnabled(false);
         add1=(ImageView)findViewById(R.id.add1);
+        add2=(ImageView)findViewById(R.id.add2);
+        add3=(ImageView)findViewById(R.id.add3);
+        add4=(ImageView)findViewById(R.id.add4);
         if (ContextCompat.checkSelfPermission(EmergencyContacts.this,
                 Manifest.permission.READ_CONTACTS)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -58,23 +70,66 @@ public class EmergencyContacts extends AppCompatActivity {
         }
         else
         {
+            Toast.makeText(this, "permission granted", Toast.LENGTH_SHORT).show();
             add1.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    addcontacts();
+                    addcontacts1();
                 }
             });
+            add2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    addcontacts2();
+                }
+            });
+            add3.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    addcontacts3();
+                }
+            });
+            add4.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    addcontacts4();
+                }
+            });
+
         }
 
 
     }
 
-    private void addcontacts() {
-        Intent i=new Intent();
-        i.setAction(i.ACTION_PICK);
-        i.setData(ContactsContract.Contacts.CONTENT_URI);
-        startActivityForResult(i,0);
+    private void addcontacts1() {
+        Intent i1=new Intent();
+        i1.setAction(i1.ACTION_PICK);
+        i1.setData(ContactsContract.Contacts.CONTENT_URI);
+        Toast.makeText(this, "activity started", Toast.LENGTH_SHORT).show();
+        startActivityForResult(i1,0);
     }
+    private void addcontacts2() {
+        Intent i2=new Intent();
+        i2.setAction(i2.ACTION_PICK);
+        i2.setData(ContactsContract.Contacts.CONTENT_URI);
+        Toast.makeText(this, "activity started", Toast.LENGTH_SHORT).show();
+        startActivityForResult(i2,1);
+    }
+    private void addcontacts3() {
+        Intent i3=new Intent();
+        i3.setAction(i3.ACTION_PICK);
+        i3.setData(ContactsContract.Contacts.CONTENT_URI);
+        Toast.makeText(this, "activity started", Toast.LENGTH_SHORT).show();
+        startActivityForResult(i3,2);
+    }
+    private void addcontacts4() {
+        Intent i4=new Intent();
+        i4.setAction(i4.ACTION_PICK);
+        i4.setData(ContactsContract.Contacts.CONTENT_URI);
+        Toast.makeText(this, "activity started", Toast.LENGTH_SHORT).show();
+        startActivityForResult(i4,3);
+    }
+
 
 
     @Override
@@ -104,45 +159,149 @@ public class EmergencyContacts extends AppCompatActivity {
         }
     }
 
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==0 && data!=null)
-        {
-            Uri contactUri=data.getData();
-            ContentResolver cr=getContentResolver();
-            String id="";
-            Cursor cursor= null;
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-                cursor = cr.query(contactUri, new String[]{
-                        ContactsContract.Contacts._ID,
-                        ContactsContract.Contacts.DISPLAY_NAME,
-                },null,null);
-            }
+        if(requestCode==0 && data!=null) {
+            Uri contactUri = data.getData();
+            ContentResolver cr = getContentResolver();
+            String id = "";
+            Cursor cursor;
+            cursor = cr.query(contactUri,null,null,null,null);
+
 
             try {
                 cursor.moveToFirst();
-                id=cursor.getString(0);
-                mn1.setText(cursor.getString(1));
-            }
-            finally {
+                c1.setText(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)));
+                id=cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+            } catch (Exception e) {
+                Toast.makeText(this, "exception is " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            } finally {
                 cursor.close();
             }
-            Cursor cursor1=cr.query(ContactsContract.CommonDataKinds.Email.CONTENT_URI,new String[]{ContactsContract.CommonDataKinds.Email.ADDRESS},
-                    ContactsContract.CommonDataKinds.Phone.CONTACT_ID+" =? ",new String[]{id},null);
+            Cursor cursor1 = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, new String[]{ContactsContract.CommonDataKinds.Phone.NUMBER},
+                    ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=?", new String[]{id}, null);
             try {
-                cursor.moveToFirst();
-                mn1.setText(cursor1.getString(0));
-            }
-            finally {
+                cursor1.moveToFirst();
+                mn1.setText(cursor1.getString(0).trim());
+            } catch (Exception e) {
+                Toast.makeText(this, "2nd exception is " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            } finally {
                 cursor1.close();
             }
         }
+
+        if(requestCode==1 && data!=null) {
+            Uri contactUri = data.getData();
+            ContentResolver cr = getContentResolver();
+            String id = "";
+            Cursor cursor;
+            cursor = cr.query(contactUri,null,null,null,null);
+
+
+            try {
+                cursor.moveToFirst();
+                c2.setText(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)));
+                id=cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+            } catch (Exception e) {
+                Toast.makeText(this, "exception is " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            } finally {
+                cursor.close();
+            }
+            Cursor cursor1 = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, new String[]{ContactsContract.CommonDataKinds.Phone.NUMBER},
+                    ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=?", new String[]{id}, null);
+            try {
+                cursor1.moveToFirst();
+                mn2.setText(cursor1.getString(0).trim());
+            } catch (Exception e) {
+                Toast.makeText(this, "2nd exception is " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            } finally {
+                cursor1.close();
+            }
+        }
+        if(requestCode==2 && data!=null) {
+
+            Uri contactUri = data.getData();
+            ContentResolver cr = getContentResolver();
+            String id = "";
+            Cursor cursor;
+            cursor = cr.query(contactUri,null,null,null,null);
+
+
+            try {
+                cursor.moveToFirst();
+                c3.setText(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)));
+                id=cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+            } catch (Exception e) {
+                Toast.makeText(this, "exception is " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            } finally {
+                cursor.close();
+            }
+            Cursor cursor1 = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, new String[]{ContactsContract.CommonDataKinds.Phone.NUMBER},
+                    ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=?", new String[]{id}, null);
+            try {
+                cursor1.moveToFirst();
+                mn3.setText(cursor1.getString(0).trim());
+            } catch (Exception e) {
+                Toast.makeText(this, "2nd exception is " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            } finally {
+                cursor1.close();
+            }
+        }
+        if(requestCode==3 && data!=null) {
+            Uri contactUri = data.getData();
+            ContentResolver cr = getContentResolver();
+            String id = "";
+            Cursor cursor;
+            cursor = cr.query(contactUri,null,null,null,null);
+
+
+            try {
+                cursor.moveToFirst();
+                c4.setText(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)));
+                id=cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+            } catch (Exception e) {
+                Toast.makeText(this, "exception is " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            } finally {
+                cursor.close();
+            }
+            Cursor cursor1 = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, new String[]{ContactsContract.CommonDataKinds.Phone.NUMBER},
+                    ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=?", new String[]{id}, null);
+            try {
+                cursor1.moveToFirst();
+                mn4.setText(cursor1.getString(0));
+            } catch (Exception e) {
+                Toast.makeText(this, "2nd exception is " + e.getMessage().trim(), Toast.LENGTH_SHORT).show();
+            } finally {
+                cursor1.close();
+            }
+        }
+
     }
+
+
 
     public void next(View view) {
         Intent i=new Intent(EmergencyContacts.this,HomePage.class);
         startActivity(i);
         finish();
+    }
+
+    public void rem1(View view) {
+        c1.setText("Contact-1");
+        mn1.setText(null);
+    }
+    public void rem2(View view) {
+        c2.setText("Contact-2");
+        mn2.setText(null);
+    }
+    public void rem3(View view) {
+        c3.setText("Contact-3");
+        mn3.setText(null);
+    }
+    public void rem4(View view) {
+        c4.setText("Contact-4");
+        mn4.setText(null);
     }
 }
